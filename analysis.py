7a22320c9266a9b39676dec37657e2a039905bc6
Danielle Hill <goal2code@gmail.com>
2015-11-02 05:38:21 -0500

    Added variables for dataframe

--- analysis.py
+++ analysis.py
@@ -2,3 +2,7 @@
 import pandas as pd
 import numpy as np
 
+#Create dataframe from inspection data csv
+path = 'fda_inspection_data.csv'
+columns = ['district', 'name', 'city', 'state', 'zip', 'country', 'date', 'center', 'area', 'rating']
+inspections = pd.read_csv(path, names=columns)
