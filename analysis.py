a6c6d302735334a660e30aa5bda321e856109370
Danielle Hill <goal2code@gmail.com>
2015-11-03 06:09:43 -0500

    Added comments to analysis.py

--- analysis.py
+++ analysis.py
@@ -6,3 +6,7 @@ import numpy as np
 path = 'fda_inspection_data.csv'
 columns = ['district', 'name', 'city', 'state', 'zip', 'country', 'date', 'center', 'area', 'rating']
 inspections = pd.read_csv(path, names=columns)
+#TODO: utf-8 char error
+
+#Summarize findings by type and year
+#Plot summary
