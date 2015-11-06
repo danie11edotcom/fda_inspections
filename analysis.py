5dbf3379a549c0429db3fdb225f7b3cd4103a66e
Danielle Hill <goal2code@gmail.com>
2015-11-06 18:40:00 -0500

    Removed summary

--- analysis.py
+++ analysis.py
@@ -7,7 +7,6 @@ path = 'fda_inspection_data.txt'
 columns = ['district', 'name', 'city', 'state', 'zip', 'country', 'date', 'center', 'area', 'rating']
 #http://stackoverflow.com/questions/31515626/pandas-reading-csv-files
 inspections = pd.read_csv(path, names=columns, encoding='cp1252')
-#TODO: troubleshoot utf-8 char error
 
 #Summarize findings by type and year
 
