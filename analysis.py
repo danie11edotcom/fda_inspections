3b7a74dffc87de60f136db752e463c40c721930e
Danielle Hill <goal2code@gmail.com>
2015-11-06 18:34:03 -0500

    Added encoding type to read csv

--- analysis.py
+++ analysis.py
@@ -5,7 +5,8 @@ import numpy as np
 #Create dataframe from inspection data csv
 path = 'fda_inspection_data.txt'
 columns = ['district', 'name', 'city', 'state', 'zip', 'country', 'date', 'center', 'area', 'rating']
-inspections = pd.read_csv(path, names=columns)
+#http://stackoverflow.com/questions/31515626/pandas-reading-csv-files
+inspections = pd.read_csv(path, names=columns, encoding='cp1252')
 #TODO: troubleshoot utf-8 char error
 
 #Summarize findings by type and year
