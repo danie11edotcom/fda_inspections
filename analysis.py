f1891c5e42f9a88a2ace06c60a2cc1c7ceab157d
Danielle Hill <goal2code@gmail.com>
2015-11-07 12:36:48 -0500

    Changed data source to xlsx file

--- analysis.py
+++ analysis.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 
 #Create dataframe from inspection data csv
-path = 'fda_inspection_data.txt'
+path = 'fda_inspections.xlsx'
 columns = ['district', 'name', 'city', 'state', 'zip', 'country', 'date', 'center', 'area', 'rating']
 #http://stackoverflow.com/questions/31515626/pandas-reading-csv-files
-inspections = pd.read_csv(path, names=columns, encoding='cp1252')
+inspections = pd.read_excel(path, sheetname='Final')
 
 #Summarize findings by type and year
 
