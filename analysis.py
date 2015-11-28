57e55ecad81f79ac0ae788b34e7b2ea50029c9df
Danielle Hill <goal2code@gmail.com>
2015-11-28 15:20:28 -0500

    Edited comments

--- analysis.py
+++ analysis.py
@@ -1,10 +1,9 @@
-#Import libraries
 import pandas as pd
 import numpy as np
 import datetime as dt
 import matplotlib as mp
 
-#Create dataframe from inspection data csv
+#Create dataframe from inspection data xlsx file from FDA site
 path = 'fda_inspections.xlsx'
 inspections = pd.read_excel(path, sheetname='Final')
 
@@ -23,16 +22,15 @@ inspections['one'] = 1
 less_2008 = inspections[inspections.year != '2008']
 full_years = less_2008[less_2008.year != '2015']
 
-#Plot summary of individual center ratings by year
+#Summarize by center
 centers = ['CBER','CDER', 'CDRH', 'CVM','CFSAN']
-# for each item in list subsitite center_name with list
-# 		total_insp_center.query('center == ["CVM"]').plot()
 
 #CBER summary
 #create dataframe for cber inspections 
 cber = full_years[full_years.center == 'CBER']
 #summarize ratings by year
 cber_year = pd.pivot_table(cber, index='year', columns='rating', values='one', aggfunc=sum)
+
 #add function to add sum and percentage for year
 def add_percent(table):
 		table['sum'] = (table['NAI']) + (table['OAI']) + (table['VAI'])
@@ -40,16 +38,21 @@ def add_percent(table):
 		table['OAIp'] = (table['OAI']) / (table['sum']) * 100
 		table['VAIp'] = (table['VAI']) / (table['sum']) * 100
 
-
-#plot and save image
-#cber_year.plot()
-#cber_year.to_json(date_format='iso', date_unit='s')
-
 #summarize ratings by year and project area
 cber_area = pd.pivot_table(cber, index=['year','area'], columns='rating', values='one', aggfunc=sum)
 #summarize ratings by year, project area and district
 cber_district = pd.pivot_table(cber, index=['year','area', 'district'], columns='rating', values='one', aggfunc=sum)
 
+
+
+
+
+
+
+#######################################################################
+#plot and save image
+#cber_year.plot()
+#cber_year.to_json(date_format='iso', date_unit='s')
 
 # #Count total inspections by year
 # total_insp = pd.pivot_table(full_years, index='year', values='one', aggfunc=sum)
