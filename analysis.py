c967af89a6310fc3c2d2a68875a47cc3bdb785ef
Danielle Hill <goal2code@gmail.com>
2015-11-28 16:17:52 -0500

    Rearranged function definition for center summaries

--- analysis.py
+++ analysis.py
@@ -25,12 +25,6 @@ full_years = less_2008[less_2008.year != '2015']
 #Summarize by center
 centers = ['CBER','CDER', 'CDRH', 'CVM','CFSAN']
 
-#CBER summary
-#create dataframe for cber inspections 
-cber = full_years[full_years.center == 'CBER']
-#summarize ratings by year
-cber_year = pd.pivot_table(cber, index='year', columns='rating', values='one', aggfunc=sum)
-
 #add function to add sum and percentage for year
 def add_percent(table):
 		table['sum'] = (table['NAI']) + (table['OAI']) + (table['VAI'])
@@ -38,12 +32,22 @@ def add_percent(table):
 		table['OAIp'] = (table['OAI']) / (table['sum']) * 100
 		table['VAIp'] = (table['VAI']) / (table['sum']) * 100
 
+#CBER summary
+#create dataframe for cber inspections 
+cber = full_years[full_years.center == 'CBER']
+add_percent(cber)
+
+#summarize ratings by year
+cber_year = pd.pivot_table(cber, index='year', columns='rating', values='one', aggfunc=sum)
+add_percent(cber_year)
+
 #summarize ratings by year and project area
 cber_area = pd.pivot_table(cber, index=['year','area'], columns='rating', values='one', aggfunc=sum)
+add_percent(cber_area)
+
 #summarize ratings by year, project area and district
 cber_district = pd.pivot_table(cber, index=['year','area', 'district'], columns='rating', values='one', aggfunc=sum)
-
-
+add_percent(cber_district)
 
 
 
